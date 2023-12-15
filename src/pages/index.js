abbc4aeab332c959e669e2ff70515b577ee958f3
Nicolas Castro Giraldo <nicolas.castro9219@gmail.com>
2023-12-14 21:08:23 -0500

    creando proyecto gatsby js

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import * as styles from "../components/index.module.css"
+
+
+export const Head = () => <Seo title="Home" />
+
+export default IndexPage
